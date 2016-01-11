662d9f95a3393f93a72511cb0e85ea8269d77c5d
Michael Baker <michael.baker96@hotmail.com>
2016-01-11 13:22:38 +1100

    Teams
    - teams are determined by the netID (even netIDs are one team, odd
    netIDs are another)
    - mainly uses the shootservermanager script
    - passes who shot the bullet through the functions
    - the shooter's team is passed to the bullet
    - the bullet's team is then compared to the player is collides with

---
+++ SPG_V0.1.7/Assets/Scripts/Teams.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Teams : NetworkBehaviour {
+	int teamVal;
+	[SyncVar]
+	public int team;
+	// Use this for initialization
+	void Start () {
+		teamVal = int.Parse(GetComponent<NetworkIdentity> ().netId.ToString ());
+		if (isLocalPlayer) {
+			CmdAssignTeam ();
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	[Command]
+	void CmdAssignTeam(){
+		RpcAssignTeam ();
+	}
+
+	[ClientRpc]
+	void RpcAssignTeam(){
+
+		if (teamVal % 2 == 0) {
+			team = 1;
+		} else {
+			team = 2;
+		}
+
+	}
+}
