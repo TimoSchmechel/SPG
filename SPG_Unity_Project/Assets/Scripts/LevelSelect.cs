d29bd3451ecf42b268d2bb52bd0a440049aea08b
AVataRR626 <matt.cabanag@gmail.com>
2016-02-12 15:20:47 +1100

    basic level selection
    -added Andrew's new mine level as a basic level (unconfigured)
    -added basic level selection functionality on the start menu

---
+++ SPG_Unity_Project/Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,44 @@
+﻿//level selection class..
+//use with GUI system to select different levels.
+
+using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour
+{
+    public InputField nameInput;
+    public string [] levels;
+    public int levelIndex = 0;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    //hook this into a GUI element to change levels
+    public void SetLevelIndex(int i)
+    {
+        levelIndex = i;
+    }
+
+    public void LoadLevel(int i)
+    {
+        SetLevelIndex(i);
+        LoadLevel();
+    }
+
+    public void LoadLevel()
+    {
+        PlayerPrefs.SetString(GlobalScript.ppPlayerNameKey, nameInput.text);
+        SceneManager.LoadScene(levels[levelIndex]);
+    }
+}
