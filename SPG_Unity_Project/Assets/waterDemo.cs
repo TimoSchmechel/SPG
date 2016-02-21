4f12eb1a43aa3125976f4611b8a3e98a3c797564
AVataRR626 <matt.cabanag@gmail.com>
2016-02-21 13:22:53 +1100

    river and boat levels
    and mine tweaks

---
+++ SPG_Unity_Project/Assets/waterDemo.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class waterDemo : MonoBehaviour {
+
+	public Material water;
+	private float speed = 0.05f;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float offset = Time.time * speed;
+		water.SetTextureOffset ("_MainTex", new Vector2 (offset/2, offset/2));
+		water.SetTextureOffset ("_DetailAlbedoMap", new Vector2 (offset*2, offset*2));
+	}
+}
